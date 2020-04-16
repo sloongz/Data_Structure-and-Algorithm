1dbee120a5bb776f92601c763b14c0a268471749
sean <sean.shi@ampedrftech.com>
2020-04-16 13:10:29 +0800

    add list

---
+++ 2-list/doubly_linke_list.c
@@ -0,0 +1,6 @@
+#include "doubly_linke_list.h"
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
---
+++ 2-list/single_link_list.c
@@ -0,0 +1,6 @@
+#include "single_link_list.h"
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
