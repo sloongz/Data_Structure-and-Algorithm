157d8ed6aeb82c88aab13bf3251fb528148bda4e
sean <sean.shi@ampedrftech.com>
2020-04-22 19:07:18 +0800

    add list queue

---
+++ 4-queue/list_circlequeue.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list_circlequeue.h"
+
+list_circlequeue_t *list_circlequeue_init()
+{
+	list_circlequeue_t *queue;
+
+	queue = (list_circlequeue_t *)malloc(sizeof(list_circlequeue_t));
+	if (queue == NULL)
+		return NULL;
+
+	queue->head = NULL;
+
+	return queue;
+}
+
+int list_circlequeue_free(list_circlequeue_t *queue)
+{
+	list_node_t *pnode; 
+
+	if (queue == NULL)
+		return -1;
+
+	while (queue->head != NULL) {
+		pnode = queue->head;
+		queue->head = pnode->next;
+		free(pnode);
+	}
+
+	free(queue);
+
+	return 0;
+}
+
+int list_circlequeue_enqueue(list_circlequeue_t *queue, int val)
+{
+	list_node_t *pnew;
+	list_node_t *p;
+
+	pnew = (list_node_t *)malloc(sizeof(list_node_t));
+	if (pnew == NULL)
+		return -1;
+	pnew->value = val;
+
+	if (queue->head == NULL) {
+		queue->head = pnew;
+		return 0;
+	}
+
+	p = queue->head;
+	while (p->next != NULL) {
+		p = p->next;
+	}
+	
+	p->next = pnew;
+
+	return 0;
+}
+
+int list_circlequeue_dequeue(list_circlequeue_t *queue)
+{
+	int ret;
+	list_node_t *p;
+
+	if (queue == NULL)
+		return -1;
+
+	p = queue->head;
+	queue->head = p->next;
+	ret = p->value;
+	free(p);
+
+	return ret;
+}
+
+int main()
+{
+	list_circlequeue_t *queue;
+
+	queue = list_circlequeue_init();
+	list_circlequeue_enqueue(queue, 1);
+	list_circlequeue_enqueue(queue, 2);
+	list_circlequeue_enqueue(queue, 3);
+	list_circlequeue_enqueue(queue, 4);
+	list_circlequeue_enqueue(queue, 5);
+
+	printf("dequeue:%d\n", list_circlequeue_dequeue(queue));
+	printf("dequeue:%d\n", list_circlequeue_dequeue(queue));
+	printf("dequeue:%d\n", list_circlequeue_dequeue(queue));
+	printf("dequeue:%d\n", list_circlequeue_dequeue(queue));
+	printf("dequeue:%d\n", list_circlequeue_dequeue(queue));
+
+	list_circlequeue_free(queue);
+	return 0;
+}
---
+++ 4-queue/list_circlequeue.h
@@ -0,0 +1,19 @@
+#ifndef _LIST_CIRCLEQUEUE_H_
+#define _LIST_CIRCLEQUEUE_H_
+
+typedef struct list_node {
+	struct list_node *next;
+	int value;
+} list_node_t;
+
+typedef struct list_circlequeue {
+	list_node_t *head;
+	int size;
+} list_circlequeue_t;
+
+list_circlequeue_t *list_circlequeue_init();
+int list_circlequeue_free(list_circlequeue_t *queue);
+int list_circlequeue_enqueue(list_circlequeue_t *queue, int val);
+int list_circlequeue_dequeue(list_circlequeue_t *queue);
+
+#endif //_LIST_CIRCLEQUEUE_H_
