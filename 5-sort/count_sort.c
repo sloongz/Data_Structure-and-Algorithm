57f55f597381bc959f4d6b81d2234180245c3768
sean <sean.shi@ampedrftech.com>
2020-06-08 16:17:28 +0800

    modify count sort

--- 5-sort/count_sort.c
+++ 5-sort/count_sort.c
@@ -3,14 +3,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE 20
+#define SIZE 10
 
 void count_sort(int *array, int len)
 {
 	int min_val;
 	int max_val;
-	int i, sum;
+	int i;
 	int *arr, *result_arr, size;
+
+	result_arr = malloc(sizeof(int)*len);
+	memset(result_arr, 0, sizeof(int)*len);
 
 	min_val = array[0];
 	for (i=0; i<len; i++) {
@@ -31,41 +34,29 @@ void count_sort(int *array, int len)
 	arr = (int *)malloc(sizeof(int)*size);
 	memset(arr, 0, sizeof(int)*size);
 
-	//input val
+	// 将数据出现的次数存储在arr[]中
 	for (i=0; i<len; i++) {
 		arr[array[i]-min_val]++;
 	}
 
-	//for (i=0; i<size; i++) {
-	//	printf("index-%d : val-%d\n", i, arr[i]);
-	//}
-
-	//sum
-	for (sum=0, i=0; i<size; i++) {
-		sum += arr[i];
-		arr[i] = sum;
+	// 更改arr[i]。目的是让更改后的arr[i]的值，是该数据在result_arr[]中的位置。
+	for (i=1; i<size; i++) {
+		arr[i] += arr[i-1];
 	}
 
-	//for (i=0; i<size; i++) {
-	//	printf("-index-%d : val-%d\n", i, arr[i]);
-	//}
-
-	result_arr = malloc(sizeof(int)*len);	
-
-	//sort
-	for (i=0; i<len; i++) {
+	//将数据存储到临时数组result_arr[]中
+	for (i=len-1; i>=0; i--) {
 		result_arr[arr[array[i]-min_val] - 1] = array[i];
 		arr[array[i]-min_val]--;
 	}
 
-	//copy
+	//将排序好的数据赋值给array[]
 	for (i=0; i<len; i++) {
 		array[i] = result_arr[i];
 	}
 
 	free(result_arr);
 	free(arr);
-
 }
 
 
@@ -76,7 +67,7 @@ int main()
 
 	srand((int)time(NULL));
 	for (i=0; i<sizeof(array)/sizeof(int); i++) {
-		array[i] = rand()%20;
+		array[i] = rand()%1000;
 		printf("%d\n", array[i]);
 	}
 
