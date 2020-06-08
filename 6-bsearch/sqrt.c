a3e8a897ebb77ee90ded96e302573928c0c201b0
sean <sean.shi@ampedrftech.com>
2020-06-08 18:27:24 +0800

    add log

--- 6-bsearch/sqrt.c
+++ 6-bsearch/sqrt.c
@@ -27,6 +27,8 @@ double msqrt(double num)
 	return mid;
 }
 
+
+//https://zh.wikipedia.org/wiki/%E5%B9%B3%E6%96%B9%E6%A0%B9%E5%80%92%E6%95%B0%E9%80%9F%E7%AE%97%E6%B3%95
 float Q_rsqrt( float number )
 {
 	long i;
